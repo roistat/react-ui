4a22492e48449b878602520e068d41f7e632fcf4
Maks <Sam1812@yandex.ru>
2016-07-27 17:58:58 +0300

    added const props

--- src/FontIcon/FontIcon.jsx
+++ src/FontIcon/FontIcon.jsx
@@ -10,6 +10,7 @@ export default class FontIcon extends React.Component {
         name: PropTypes.string, 
     };
     render() {
+		const props = this.props;
         const classNameFromProps = (props.className ? ` ${props.className}` : '');
         const className = classNameFromProps;
         return (

f8e416601770db6d2d38480776dc6be5b61dffd3
Maks <Sam1812@yandex.ru>
2016-07-28 12:45:03 +0300

    commit

--- src/FontIcon/story.jsx
+++ src/FontIcon/story.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import FontIcon from './FontIcon.jsx';
-	storiesOf('FontIcon', module)
+storiesOf('FontIcon', module)
     .addWithInfo('FontIconCheckbox', 'FontIcon Checkbox', () => (
         <FontIcon name="check" className="checkbox"/>
     ))	

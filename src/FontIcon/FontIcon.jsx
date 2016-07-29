5bf316294816dd4aecca123ac5eae4a67d09c0e3
Maks <Sam1812@yandex.ru>
2016-07-29 10:48:02 +0300

    Поправил отступы

--- src/FontIcon/FontIcon.jsx
+++ src/FontIcon/FontIcon.jsx
@@ -3,21 +3,21 @@
 import React, { PropTypes } from 'react';
 
 export default class FontIcon extends React.Component {
-    static propTypes = {
-		/**
-		 * Name of font awesome icon without 'fa-' prefix
-		 */
-        name: PropTypes.string, 
-    };
-    render() {
-	 const props = this.props;	
-        const classNameFromProps = (props.className ? ` ${props.className}` : '');
-        const className = classNameFromProps;
-        return (
-            <i
-                {...props}
-                className={`fa fa-${props.name}${className}`}
-            />
-        )
-    }
+	static propTypes = {
+	/**
+	 * Name of font awesome icon without 'fa-' prefix
+	 */
+		name: PropTypes.string,
+	};
+	render() {
+		const props = this.props;
+		const classNameFromProps = (props.className ? ` ${props.className}` : '');
+		const className = classNameFromProps;
+		return (
+			<i
+				{...props}
+				className={`fa fa-${props.name}${className}`}
+			/>
+		)
+	}
 };

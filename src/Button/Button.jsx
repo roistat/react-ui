576754e03ea530233e663eac8bb455bdb93554dc
fi11 <pavelsilinw@gmail.com>
2016-08-18 11:59:18 +0300

    refactoring and add text component

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -5,7 +5,6 @@ import AbstractButton from '../AbstractButton';
 
 import { FONT, COLOR, SHADOW } from '../const/theme';
 
-import Color from 'color';
 import { StyleSheet, css } from '../helpers/styles';
 
 export default class Button extends AbstractButton {
@@ -53,26 +52,55 @@ export default class Button extends AbstractButton {
         /**
          * Button size
          */
-        size: PropTypes.oneOf(['l', 'm', 's', 'xs'])
+        size: PropTypes.oneOf(['l', 'm', 's', 'xs']),
+        /**
+         * Show waiting status
+         */
+        isWaiting: PropTypes.bool
     };
 
     static defaultProps = {
         size: 'm'
     };
 
+    _getChildren() {
+        const { children } = this.props;
+        const result = [];
+        const count = React.Children.count(children);
+
+        if (count === 1) {
+            return children;
+        }
+
+        React.Children.forEach(this.props.children, (item, idx) => {
+            result.push(item);
+
+            if (idx !== (count - 1)) {
+                result.push('\u00a0');
+            }
+        });
+
+        return result;
+    }
+
     render() {
         const props = this.props;
-
+        
         return (
             <button
                 {...this.getDefaultRenderProps()}
-                className={
-                    css(
-                        ...this.buildStyleList(styles, 'button'),
-                        styles[`buttonSize${props.size.toUpperCase()}`],
-                        ...(props.styles || [])
-                        )}>
-                {props.children}
+                className={css(...this.buildStyleList(styles, 'button'), styles.getPreset('size', props.size))}>
+                <View styles={[styles.content, props.isWaiting && styles.hiddenContent]}>
+                    {this._getChildren()}
+                    {props.isWaiting &&
+                        <View className={css(styles.spinner)}>
+                            <Spinner
+                                size={props.size === 'xs' ? 'tiny' : 'xs'}
+                                color={props.isDisabled ? COLOR.MUTED : COLOR.PRIMARY}
+                            />
+                        </View>
+                    }
+                </View>
             </button>
         )
     }
@@ -102,34 +130,63 @@ const styles = StyleSheet.create({
             boxShadow: 'inset 0px 2px 0px 0px rgba(27, 42, 48, 0.1), inset 0px -1px 0px 0px #b4bdc0, inset 1px 1px 0px 0px #c6cbce, inset -1px 0px 0px 0px #c6cbce'
         }
     },
+    content: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center'
+    },
     buttonIsFocused: {
         boxShadow: `${SHADOW.FOCUS}, inset 0px -2px 0px 0px rgba(27, 42, 48, 0.1), inset 0px -1px 0px 0px #b4bdc0, inset 1px 1px 0px 0px #c6cbce, inset -1px 0px 0px 0px #c6cbce;`
     },
-    buttonSizeL: {
+    buttonIsDisabled: {
+        cursor: 'default',
+        transition: 'none',
+        color: COLOR.MUTED,
+        border: '1px solid #c3cacd',
+        boxShadow: 'none',
+        background:  COLOR.DISABLED,
+        '&:hover': {
+            cursor: 'default',
+            border: '1px solid #c3cacd',
+            boxShadow: 'none',
+            background:  COLOR.DISABLED,
+        },
+        '&:active': {
+            cursor: 'default',
+            border: '1px solid #c3cacd',
+            boxShadow: 'none',
+            background:  COLOR.DISABLED,
+        }
+    },
+    sizeL: {
         padding: '.3rem .6rem',
         minWidth: '1.8rem',
         height: '1.8rem'
     },
-    buttonSizeM: {
+    sizeM: {
         padding: '.2rem .6rem',
         minWidth: '1.6rem',
         height: '1.6rem'
     },
-    buttonSizeS: {
+    sizeS: {
         padding: '.1rem .4rem',
         minWidth: '1.4rem',
         height: '1.4rem'
     },
-    buttonSizeXS: {
+    sizeXs: {
         fontSize: FONT.SIZE.BUTTON_SMALL_TEXT,
         padding: '0 .4rem',
         minWidth: '1.2rem',
         height: '1.2rem'
     },
-    buttonIsDisabled: {
-        transition: 'none',
-        color: COLOR.MUTED,
-        background: 'linear-gradient(to top, rgb(0,159,101) 0%, rgb(1,171,109) 100%)',
-        boxShadow: 'inset 0px -2px 0px 0px rgba(27, 42, 48, 0.1), inset 0px -1px 0px 0px #b4bdc0, inset 1px 1px 0px 0px #c6cbce, inset -1px 0px 0px 0px #c6cbce;',
+    hiddenContent: {
+        visibility: 'hidden'
+    },
+    spinner: {
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: 'translate3d(-50%, -50%, 0)',
+        visibility: 'visible'
     }
 });
---
+++ src/Text/Readme.md
@@ -0,0 +1,12 @@
+### Usage
+
+```js
+    import Text from '@roistat/ui/lib/Text';
+```
+
+Example
+
+    <Text>
+        Some awesome text
+    </Text>
+    

8640e6d0f842f1740f00e3e7457ee238e94a54b6
Maks <Sam1812@yandex.ru>
2016-08-25 13:33:42 +0300

    SVGIcons

--- src/SVGIcons/ChevronIcon.jsx
+++ src/SVGIcons/ChevronIcon.jsx
@@ -3,9 +3,10 @@
 import React from 'react';
 
 const ChevronIcon = (props) => (
-    <svg width='16' height='16' viewBox='0 0 16 16'>
-        <path fill='none' stroke={props.color} d='M3 5.797l5 5 5-5'/>
-    </svg>
+	<svg width='16' height='16' viewBox='0 0 16 16'>
+		<path fill='none' stroke={props.color} d='M3 5.797l5 5 5-5'/>
+	</svg>
 );
 
 export default ChevronIcon;
+

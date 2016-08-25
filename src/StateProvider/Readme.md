bb6af73d4c6537f16eb27014b80afec6646a8213
Maks <Sam1812@yandex.ru>
2016-08-25 12:32:30 +0300

    StateProvider

--- src/StateProvider/Readme.md
+++ src/StateProvider/Readme.md
@@ -1,15 +1,16 @@
 StateProvider usage
-        
-    <StateProvider state={{ content: 'awesome content' }}>
-        {(state, setState) => (
-            <div>
-                <button onClick={() => setState({ content: 'new content' })}>
-                    update
-                </button> 
-                {"\u00a0"}
-                <span>
-                    {state.content}
-                </span>
-            </div>
-        )}
-    </StateProvider>
+
+	<StateProvider state={{ content: 'awesome content' }}>
+		{(state, setState) => (
+			<div>
+				<button onClick={() => setState({ content: 'new content' })}>
+					update
+				</button> 
+				{"\u00a0"}
+				<span>
+					{state.content}
+				</span>
+			</div>
+		)}
+	</StateProvider>
+
--- src/StateProvider/StateProvider.jsx
+++ src/StateProvider/StateProvider.jsx
@@ -3,36 +3,36 @@
 import React, { PropTypes } from 'react';
 
 export default class StateProvider extends React.Component {
-    static propTypes = {
-        /**
-         * Initial state
-         */
-        state: PropTypes.object,
-        /**
-         * Wrapper function
-         */
-        children: PropTypes.func.isRequired
-    };
+	static propTypes = {
+		/**
+		 * Initial state
+		 */
+		state: PropTypes.object,
+		/**
+		 * Wrapper function
+		 */
+		children: PropTypes.func.isRequired
+	};
 
-    static defaultProps = {
-        state: {}
-    };
+	static defaultProps = {
+		state: {}
+	};
 
-    constructor(props, context) {
-        super(props, context);
+	constructor(props, context) {
+		super(props, context);
+		this.state = props.state || {};
+	}
 
-        this.state = props.state || {};
-    }
+	render() {
+		if (typeof this.props.children !== 'function') {
+			throw new Error('StateProvider component: children must be a function');
+		}
 
-    render() {
-        if (typeof this.props.children !== 'function') {
-            throw new Error('StateProvider component: children must be a function');
-        }
-
-        return (
-            <div>
-               {this.props.children(this.state, (...args) => this.setState(...args))}
-            </div>
-        )
-    }
+		return (
+			<div>
+				{this.props.children(this.state, (...args) => this.setState(...args))}
+			</div>
+		)
+	}
 }
+

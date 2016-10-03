6a377d80b39fb701dcee75fe9c2f387e0f31ffee
Kanonir87 <Kanonir87@gmail.com>
2016-10-03 19:57:13 +0300

    Update story.jsx

--- src/Checkbox/story.jsx
+++ src/Checkbox/story.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Checkbox from './Checkbox';
 import View from '../View';
+import StateProvider from '../StateProvider';
 
 storiesOf('Checkbox', module)
     .add('Checkbox', () => (
         <View>
-            <Checkbox />
-        </View>
+			<StateProvider state={{ isChecked: false }}>
+				{(state, setState) => (
+					<div>
+						<Checkbox onClick={() => setState({ isChecked: !state.isChecked })} />
+						<span> {state.isChecked ? 'Вкл' : 'Выкл'}</span>
+					</div>
+					)}
+			</StateProvider>	
+		</View>
     ))
     .add('Checkbox not toggle mode', () => (
-        <View>
-            <Checkbox isToggleMod={false} />
-        </View>
+        <Checkbox isToggleMod={false} />
     ));
 

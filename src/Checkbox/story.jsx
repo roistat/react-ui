c250827efb711b30c6e53343ad6c8034235596d7
Kanonir87 <Kanonir87@gmail.com>
2016-10-04 09:28:22 +0300

    Update story.jsx

--- src/Checkbox/story.jsx
+++ src/Checkbox/story.jsx
@@ -7,15 +7,15 @@ import StateProvider from '../StateProvider';
 storiesOf('Checkbox', module)
     .add('Checkbox', () => (
         <View>
-			<StateProvider state={{ isChecked: false }}>
-				{(state, setState) => (
-					<div>
-						<Checkbox onClick={() => setState({ isChecked: !state.isChecked })} />
-						<span> {state.isChecked ? 'Вкл' : 'Выкл'}</span>
-					</div>
-					)}
-			</StateProvider>	
-		</View>
+            <StateProvider state={{ isChecked: false }}>
+                {(state, setState) => (
+                    <div>
+                        <Checkbox onClick={() => setState({ isChecked: !state.isChecked })} />
+                        <span> {state.isChecked ? 'Вкл' : 'Выкл'}</span>
+                    </div>
+                    )}
+            </StateProvider>
+        </View>
     ))
     .add('Checkbox not toggle mode', () => (
         <Checkbox isToggleMod={false} />

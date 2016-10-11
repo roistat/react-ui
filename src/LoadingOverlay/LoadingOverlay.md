1770479d9af61e09afe25d49af08d7eb2318fdbe
Maks <Sam1812@yandex.ru>
2016-10-11 17:46:34 +0300

    LoadingOverlay

--- src/LoadingOverlay/LoadingOverlay.md
+++ src/LoadingOverlay/LoadingOverlay.md
@@ -9,6 +9,6 @@ const View = require('../View').default;
 *LoadingOverlay*
 
     <View>
-        <LoadingOverlay />
-        <button style={{width:'100px', margin:'30% auto'}}>Button</button>
+        <button onClick={() => setState({ isShown: true })}>Button</button>
+        { state.isShown && <LoadingOverlay /> }
     </View>
--- src/LoadingOverlay/story.jsx
+++ src/LoadingOverlay/story.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import LoadingOverlay from './LoadingOverlay';
 import View from '../View';
+import StateProvider from '../StateProvider';
 
 storiesOf('LoadingOverlay', module)
     .add('LoadingOverlay', () => (
         <View>
-            <LoadingOverlay />
-            <button style={{width:'100px', margin:'30% auto'}}>Button</button>
+            <StateProvider>
+                {(state, setState) => (
+                    <div>
+                        <button onClick={() => setState({ isShown: true })}>Button</button>
+                        { state.isShown && <LoadingOverlay /> }
+                    </div>
+                    )}
+            </StateProvider>
         </View>
     ));

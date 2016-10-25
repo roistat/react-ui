e9e87d3531ca2a070c2ab78b1f7041464f17ad80
Kanonir87 <Kanonir87@gmail.com>
2016-10-25 11:08:55 +0300

    Update story.jsx

--- src/Switcher/story.jsx
+++ src/Switcher/story.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Switcher from './Switcher';
 import View from '../View';
-import StateProvider from '../StateProvider';
 
 storiesOf('Switcher', module)
     .add('Switcher', () => (
         <View>
-            <StateProvider>
-                {(state, setState) => (
-                    <div>
-                        <Switcher onClick={() => setState({ isChecked: !state.isChecked })} isChecked={state.isChecked} />
-                    </div>
-                )}
-            </StateProvider>
+            <Switcher />
+        </View>
+    ))
+
+    .add('Switcher is disabled', () => (
+        <View>
+            <Switcher isDisabled={true} />
+        </View>
+    ))
+
+    .add('Switcher is checked', () => (
+        <View>
+            <Switcher isChecked={true} />
         </View>
     ));
